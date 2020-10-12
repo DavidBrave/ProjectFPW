442c95289a04795cd5befe28f449efb3aa8d421a
DavidBrave <davidbrave244@gmail.com>
2020-10-13 02:50:23 +0700

    David. Initialization.

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function ShowLogin()
+    {
+        return view("welcome");
+    }
+
+    public function Login(Request $request)
+    {
+
+    }
+}
